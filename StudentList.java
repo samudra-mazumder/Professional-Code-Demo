8363b05d0b4f0f3026e695fa06b4db7676125eb2
samudra-mazumder <samudramazumder@gmail.com>
2024-10-05 01:59:45 +0600

    Step3

--- StudentList.java
+++ StudentList.java
@@ -11,12 +11,12 @@ public class StudentList {
         if (args[0].equals("a")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader s = new BufferedReader(
+                BufferedReader bufferedReader = new BufferedReader(
 						           new InputStreamReader(
                                    new FileInputStream("students.txt")));
-                String r = s.readLine();
-                String i[] = r.split(",");
-                for (String j : i) {
+                String readLine = bufferedReader.readLine();
+                String strings[] = readLine.split(",");
+                for (String j : strings) {
                     System.out.println(j.trim());
                 }
             } catch (Exception e) {
@@ -25,30 +25,30 @@ public class StudentList {
         } else if (args[0].equals("r")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader s = new BufferedReader(
+                BufferedReader bufferedReader = new BufferedReader(
                                    new InputStreamReader(
                                    new FileInputStream("students.txt")));
-                String r = s.readLine();
+                String r = bufferedReader.readLine();
 
-                String i[] = r.split(",");
-                Random x = new Random();
-                int y = x.nextInt(i.length);
-                System.out.println(i[y]);
+                String strings[] = r.split(",");
+                Random random = new Random();
+                int nextInt = random.nextInt(strings.length);
+                System.out.println(strings[nextInt]);
             } catch (Exception e) {
             }
             System.out.println("Data Loaded.");
         } else if (args[0].contains("+")) {
             System.out.println("Loading data ...");
             try {
-                BufferedWriter s = new BufferedWriter(
-                                   new FileWriter("students.txt", true));
-                String t = args[0].substring(1);
-                Date d = new Date();
-                String df = "dd/mm/yyyy-hh:mm:ss a";
-                DateFormat dateFormat = new SimpleDateFormat(df);
-                String fd = dateFormat.format(d);
-                s.write(", " + t + "\nList last updated on " + fd);
-                s.close();
+                BufferedWriter bufferedWriter = new BufferedWriter(
+                                                new FileWriter("students.txt", true));
+                String substring = args[0].substring(1);
+                Date date = new Date();
+                String finalDate = "dd/mm/yyyy-hh:mm:ss a";
+                DateFormat dateFormat = new SimpleDateFormat(finalDate);
+                String formatted = dateFormat.format(date);
+                bufferedWriter.write(", " + substring + "\nList last updated on " + formatted);
+                bufferedWriter.close();
             } catch (Exception e) {
             }
 
@@ -56,15 +56,15 @@ public class StudentList {
         } else if (args[0].contains("?")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader s = new BufferedReader(
-                                   new InputStreamReader(
-                                   new FileInputStream("students.txt")));
-                String r = s.readLine();
-                String i[] = r.split(",");
+                BufferedReader bufferedReader = new BufferedReader(
+                                                new InputStreamReader(
+                                                new FileInputStream("students.txt")));
+                String readLine = bufferedReader.readLine();
+                String split[] = readLine.split(",");
                 boolean done = false;
-                String t = args[0].substring(1);
-                for (int idx = 0; idx < i.length && !done; idx++) {
-                    if (i[idx].trim().equals(t)) {
+                String string = args[0].substring(1);
+                for (int idx = 0; idx < split.length && !done; idx++) {
+                    if (split[idx].trim().equals(string)) {
                         System.out.println("We found it!");
                         done = true;
                     }
